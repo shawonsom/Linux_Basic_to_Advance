810e5a523d523ab2ae291b9b430d072600610a86
Shahriar shawon <shawonsom@gmail.com>
2025-05-18 13:24:56 +0600

    folder_backup.sh
    change location

--- folder_backup.sh
+++ folder_backup.sh
@@ -4,13 +4,14 @@ DATADIR=/ismp/test
 DATADIR1=/var/www/
 DATADIR2=/etc
 DATADIR3=/home/database_backup
-BACKUPDIR=/NAS/Mybackup/BLink-22
+BACKUPDIR=/NAS/Mybackup/local_host_backup
 TODAYPATH=${BACKUPDIR}/${TODAY}
 if [[ ! -e ${TODAYPATH} ]]; then
         mkdir -p ${TODAYPATH}
 fi
 
+# --exclude means ignore it  & -mtime +2 means it keeps 3 days data
 rsync -aze --link-dest --exclude '*.txt' --exclude 'core.*'  ${DATADIR} ${DATADIR1} ${DATADIR2} ${DATADIR3} ${TODAYPATH}
 tar -zcf ${BACKUPDIR}/$TODAY.tar.gz -P ${BACKUPDIR}/$TODAY
 rm -rf ${BACKUPDIR}/$TODAY
-find /NAS/Mybackup/BLink-22/* -mtime +1 -exec rm {} \;
+find /NAS/Mybackup/local_host_backup/* -mtime +2 -exec rm {} \;
