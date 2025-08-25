1f995159ab8f971c71b8e20ecbc90adc5985093d
shawonsom <shawonsom@gmail.com>
2025-08-25 11:53:56 +0600

    empty

--- auto_archive.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Directory to search for .txt.gz files
-SEARCH_DIR="/var/www/html/sdp/logs"
-
-# Find folders older than 3 days
-find "$SEARCH_DIR" -type d -name "*" -mtime +3 -exec gzip -r {} \;
